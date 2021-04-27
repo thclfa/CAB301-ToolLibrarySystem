3b596445cf26efbda50e5a62f527d8c05a535fac
Thomas <N10582835@qut.edu.au>
2021-04-27 10:44:53 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assessment1_ToolLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
